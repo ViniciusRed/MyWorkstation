2e6f53e07ad0cf57492a90d923c45b19ff025ad7
这不是 Hack 而是技能 <81523685+ViniciusRed@users.noreply.github.com>
2022-08-02 10:11:11 -0300

    Update Config.sh

--- Config.sh
+++ Config.sh
@@ -1,2 +1,8 @@
 sudo pacman -Sy
-wait code
+timedatectl
+timedatectl set-local-rtc 0 #off to sync time
+timedatectl set-timezone America/Sao_Paulo
+sudo timedatectl set-ntp true
+timedatectl set-local-rtc 1
+git clone https://aur.archlinux.org/yay.git && cd yay
+makepkg -si
