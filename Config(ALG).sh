fec14bd99201399b57e0b5b914217665cb43daf8
Vinicius_Red <atalibavinicius@gmail.com>
2022-08-05 22:51:08 -0300

    Update

--- Config(ALG).sh
+++ Config(ALG).sh
@@ -1,9 +1,10 @@
-sudo pacman -Sy
+sudo pacman -Sy && pacman -Syy
 timedatectl
 timedatectl set-local-rtc 0 #off to sync time
 timedatectl set-timezone America/Sao_Paulo
 sudo timedatectl set-ntp true
 timedatectl set-local-rtc 1
+sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/yay.git && cd yay
 makepkg -si
 clear
