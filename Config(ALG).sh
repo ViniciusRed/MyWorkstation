d1bd28cc0d138080530e31887a891d4cc836d964
这不是 Hack 而是技能 <81523685+ViniciusRed@users.noreply.github.com>
2022-08-11 17:09:31 -0300

    Update

--- Config(ALG).sh
+++ Config(ALG).sh
@@ -1,4 +1,4 @@
-sudo pacman -Sy && pacman -Syy && pacman -Syyu
+sudo pacman -Sy && pacman -Syy
 timedatectl
 timedatectl set-local-rtc 0 #off to sync time
 timedatectl set-timezone America/Sao_Paulo
@@ -8,5 +8,4 @@ sudo pacman -S file-roller
 sudo pacman -S unrar
 git clone https://aur.archlinux.org/yay.git && cd yay
 makepkg -si
-echo "[please delete yay folder]"
 clear
