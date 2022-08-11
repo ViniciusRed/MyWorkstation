9fddcaaa22d9b07f9ae8e37dd9155c156b9545ce
这不是 Hack 而是技能 <81523685+ViniciusRed@users.noreply.github.com>
2022-08-11 16:06:14 -0300

    Update Config(ALG).sh

--- Config(ALG).sh
+++ Config(ALG).sh
@@ -1,11 +1,11 @@
-sudo pacman -Sy && pacman -Syy
+sudo pacman -Sy && pacman -Syy && pacman -Syyu
 timedatectl
 timedatectl set-local-rtc 0 #off to sync time
 timedatectl set-timezone America/Sao_Paulo
 sudo timedatectl set-ntp true
 timedatectl set-local-rtc 1
+sudo pacman -S file-roller
+sudo pacman -S unrar
 git clone https://aur.archlinux.org/yay.git && cd yay
 makepkg -si
 clear
-yay -S pamac-aur
-#wait code update
