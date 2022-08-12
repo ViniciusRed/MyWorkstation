fffbf3b1d2ac60a4508fe83fc12d058e9e56a103
这不是 Hack 而是技能 <81523685+ViniciusRed@users.noreply.github.com>
2022-08-12 01:55:17 -0300

    Update
    Worked perfectly

--- Config.sh
+++ Config.sh
@@ -6,6 +6,9 @@ sudo timedatectl set-ntp true
 timedatectl set-local-rtc 1
 sudo pacman -S file-roller
 sudo pacman -S unrar
+cd && cd
 git clone https://aur.archlinux.org/yay.git && cd yay
 makepkg -si
+yay -S pamac-aur
 clear
+echo "[Please restart Arch Linux]"
